64a6b202c651ff590c1343d3a9dde70515b18ee1
Varya Safonova <varyaSafonov@yandex.ru>
2023-08-09 21:35:19 +0300

    added /quiz command

--- bot.py
+++ bot.py
@@ -16,6 +16,11 @@ class State:
    POLL_TYPE = 2
    POLL = 3
    POLL_ERROR = 4
+   QUESTION_QUIZ = 5
+   ANSWERS_QUIZ = 6
+   QUIZ_TYPE = 7
+   CORRECT_ANS = 8
+   QUIZ = 9
 
 
 user_state = {}
@@ -23,6 +28,7 @@ user_state = {}
 qw_dict = {}
 ans_dict = {}
 type_dict = {}
+cor_ans_dict = {}
 
 def set_state(user_id: int, state: int):
    user_state[user_id] = state
@@ -35,16 +41,14 @@ def get_user_state(user_id: int):
 # Handler to a command /start
 @dp.message_handler(commands=['start']) 
 async def send_welcome(message: types.Message):
+   start_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+   start_keyboard.add(types.KeyboardButton(text='/fast_quiz'))
+   start_keyboard.add(types.KeyboardButton(text='/fast_poll'))
+   start_keyboard.add(types.KeyboardButton(text='/help'))
+   start_keyboard.add(types.KeyboardButton(text='/poll'))
+   start_keyboard.add(types.KeyboardButton(text='/quiz'))
 
-    kb = [
-        [
-        types.KeyboardButton(text='/fast_quiz'), 
-        types.KeyboardButton(text='/help'),
-        types.KeyboardButton(text='/fast_poll')
-        ]
-    ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
-    await message.reply('Hello!\nI am a polling bot!\nI can make polls in telegram.', reply_markup=keyboard) 
+   await message.reply('Hello!\nI am a polling bot!\nI can make polls and quizes in telegram.\nUse a /help command to see what I can do.', reply_markup=start_keyboard) 
  
 
 # Handler to a command /help
@@ -114,7 +118,6 @@ async def answers_parse(message: types.Message):
 
 
 
-
 async def type_parse(message: types.Message):
    user_id = message.from_user.id
    type_text = message.text
@@ -139,6 +142,68 @@ async def sending_poll(message: types.Message):
       user_id = message.from_user.id
       await message.answer(f"Let's start from the beggining. Send me your question.")
       set_state(user_id, State.QUESTION) 
+
+
+# Handler to a command /quiz
+@dp.message_handler(commands=["quiz"]) 
+async def poll(message: types.Message):
+   user_id = message.from_user.id
+   await message.answer("Send me your question:")
+   set_state(user_id, State.QUESTION_QUIZ)
+
+async def question_parse_quiz(message: types.Message):
+   user_id = message.from_user.id
+   qw_dict[user_id] = message.text
+   await message.answer("Send me your answers. Use ';' as a separator please.")
+   set_state(user_id, State.ANSWERS_QUIZ)
+
+
+async def answers_parse_quiz(message: types.Message):
+   user_id = message.from_user.id
+   if ';' in message.text:
+      ans_dict[user_id] = message.text
+      await message.answer('Do you want to have an anonymous quiz?\nSend "YES" or "NO".')
+      set_state(user_id, State.QUIZ_TYPE)
+   else:
+      quiz_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+      quiz_keyboard.add(types.KeyboardButton(text="/quiz"))
+      quiz_keyboard.add(types.KeyboardButton(text="Cancel"))
+      await message.reply('Sorry, wrong format. Start again, please.', reply_markup=quiz_keyboard)
+
+
+
+async def type_parse_quiz(message: types.Message):
+   user_id = message.from_user.id
+   type_text = message.text
+   if type_text == 'YES' or type_text == 'Yes' or type_text == 'yes':
+      type_dict[user_id] = True
+   else:
+      type_dict[user_id] = False
+   await message.answer('Send me a correct answer, please.')
+   set_state(user_id, State.CORRECT_ANS)
+
+async def correct_ans_parse(message: types.Message):
+   user_id = message.from_user.id
+   type_text = message.text
+   cor_ans_dict[user_id] = type_text
+   await message.answer(f'Let me see if I have understood you right:\nYour question: {qw_dict[user_id]}\nAnswers: {ans_dict[user_id]}.\nIs anonymous: {type_dict[user_id]}.\nCorrect answer: {cor_ans_dict[user_id]}.\nAnswer me "YES" if everything is correct or "NO" if you want to correct something.')
+   set_state(user_id, State.QUIZ)
+
+
+async def sending_quiz(message: types.Message):
+   type_text = message.text
+   user_id = message.from_user.id
+   if type_text == 'YES' or type_text == 'Yes' or type_text == 'yes':
+      await bot.send_poll(chat_id=user_id, 
+                       question=qw_dict[user_id],                     
+                       options=ans_dict[user_id].split(sep=';'),                     
+                       type='quiz',
+                       correct_option_id = ans_dict[user_id].split(sep=';').index(cor_ans_dict[user_id]),                     
+                       is_anonymous = type_dict[user_id])
+   elif type_text == 'NO' or type_text == 'No' or type_text == 'no':
+      user_id = message.from_user.id
+      await message.answer(f"Let's start from the beggining. Send me your question.")
+      set_state(user_id, State.QUESTION_QUIZ) 
     
 
 
@@ -153,6 +218,16 @@ async def text_handler(message: types.Message):
       await type_parse(message)
    elif get_user_state(user_id) == State.POLL: 
       await sending_poll(message)
+   elif get_user_state(user_id) == State.QUESTION_QUIZ:
+      await question_parse_quiz(message)
+   elif get_user_state(user_id) == State.ANSWERS_QUIZ:
+      await answers_parse_quiz(message)
+   elif get_user_state(user_id) == State.QUIZ_TYPE:
+      await type_parse_quiz(message)
+   elif get_user_state(user_id) == State.CORRECT_ANS: 
+      await correct_ans_parse(message)
+   elif get_user_state(user_id) == State.QUIZ: 
+      await sending_quiz(message)
    else:
       await message.answer("Something got wrong :(")
 
